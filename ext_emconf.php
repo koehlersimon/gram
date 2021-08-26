715165cf1b306531b20ae0ebebf7e9767fb26af2
koehlersimon <info@simon-koehler.com>
2021-08-26 16:12:12 -0500

    Add important files

---
+++ ext_emconf.php
@@ -0,0 +1,31 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Gram',
+    'description' => 'Social Network with TYPO3',
+    'category' => 'Content',
+    'author' => 'Simon Köhler',
+    'author_email' => 'info@simon-koehler.com',
+    'author_company' => 'simon-koehler.com',
+    'shy' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'alpha',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 0,
+    'lockType' => '',
+    'version' => '0.0.1',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '10.4.0-11.3.99'
+        ],
+        'conflicts' => [
+
+        ],
+        'suggests' => [
+
+        ],
+    ]
+];
