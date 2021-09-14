d8bfdf8f5081033862ad95f3f407f22be55deda4
koehlersimon <info@simon-koehler.com>
2021-09-13 20:25:46 -0500

    Update BackendLayouts

---
+++ Configuration/TCA/Overrides/pages.php
@@ -0,0 +1,7 @@
+<?php
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+   'gram',
+   'Configuration/TypoSript/TsConfig/Page/belayouts.tsconfig',
+   'GRAM Page Config'
+);
