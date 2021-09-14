a16ca5175fa37959ac43232ff55e6789f68a7301
koehlersimon <info@simon-koehler.com>
2021-09-14 08:27:31 -0500

    Import FE User Repo

--- Classes/Controller/InboxController.php
+++ Classes/Controller/InboxController.php
@@ -2,6 +2,7 @@
 namespace SIMONKOEHLER\Gram\Controller;
 use TYPO3\CMS\Extbase\Annotation\Inject;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Extbase\Domain\Repository\FrontendUserRepository;
 
 /**
  * MessageController
@@ -10,12 +11,27 @@ class InboxController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
 {
 
     /**
+     * @var FrontendUserRepository
+     */
+    private $frontendUserRepository;
+
+
+    /**
+     * @param FrontendUserRepository $frontendUserRepository
+     */
+    public function injectFrontendUserRepository(FrontendUserRepository $frontendUserRepository){
+        $this->frontendUserRepository = $frontendUserRepository;
+    }
+
+    /**
      * action inbox
      *
      * @return void
      */
     public function interfaceAction(){
-
+        $fe_user = $this->frontendUserRepository->findByUid($GLOBALS['TSFE']->fe_user->user['uid']);
+        $this->view->assign('user',$fe_user);
+        $this->view->assign('settings',$this->settings);
     }
 
     /**
